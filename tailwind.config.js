63c22ce7d688252413ac384d9f5ed9baa6f7d82a
Venky <82228290+venky2303@users.noreply.github.com>
2023-01-09 13:35:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      'xs':'425px',
+      'sm': '640px',
+      'md':'768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl':'1536px',
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
